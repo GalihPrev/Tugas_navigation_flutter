1b41d161c7e0e1df4141a4b937082aac63d4db67
GalihPrev <galihpreviand27@gmail.com>
2022-03-22 22:40:44 +0700

    Navigation_Flutter

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+import 'package:tugas_navigation_flutter/pages/home_page.dart';
+import 'package:tugas_navigation_flutter/pages/item_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+    initialRoute: '/',
+    routes: {'/': (context) => HomePage(), '/item': ((context) => ItemPage())},
+  ));
+}
